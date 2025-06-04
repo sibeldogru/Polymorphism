7d1ee28ce805ff7847a18901cf332c3f35c30b8b
sibeldogru <sibeldogru@gmail.com>
2025-06-04 15:48:18 +0300

    Add project files.

---
+++ Polymorphism/Models/BaseGeometrikSekil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism.Models
+{
+    internal class BaseGeometrikSekil
+    {
+        public int Genislik { get; set; }
+        public int Yukseklik { get; set; }
+
+     public virtual int AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    public void AlaniYazdir()
+
+        {
+            Console.WriteLine($"Alanı: {AlanHesapla()}");
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Polymorphism.Models;
+
+Kare kare = new Kare
+{
+    Genislik = 5,
+    Yukseklik = 5,
+};
+
+Console.WriteLine("Kare için: ");
+kare.AlaniYazdir();
+Console.WriteLine("--------------------");
+
+Dikdortgen dikdortgen = new Dikdortgen
+{
+    Genislik = 7,
+    Yukseklik = 4,
+};
+
+
+Console.WriteLine("Dikdörtgen için: ");
+dikdortgen.AlaniYazdir();
+Console.WriteLine("--------------------");
+
+DikUcgen dikucgen = new DikUcgen
+
+{
+    Genislik = 5,
+    Yukseklik = 9,
+};
+
+Console.WriteLine("DikÜçgen için: ");
+dikucgen.AlaniYazdir();
